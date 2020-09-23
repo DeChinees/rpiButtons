712cd6b0fa9df46e0ec01fdb2967d0003db27e4a
DeChinees <dchinon@yahoo.com>
2020-09-23 22:52:37 +0200

    go

---
+++ main.py
@@ -0,0 +1,15 @@
+from gpiozero import Button
+
+button1 = Button(16)
+
+
+def button_pressed():
+    while True:
+        if (button1 == 0):
+            print('button 1 pressed.')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    button_pressed()
+
