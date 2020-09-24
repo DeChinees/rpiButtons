202b05bff56dbc2ab0e0fce02501d48dcf607e86
DeChinees <dchinon@yahoo.com>
2020-09-24 17:59:43 +0200

    add buttons

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from gpiozero import Button
 
 button1 = Button(18)
+button2 = Button(23)
+button3 = Button(24)
 
-
-def button_pressed():
-    while True:
-        if button1.is_pressed:
-            print('button 1 pressed.')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    button_pressed()
-
+while True:
+    if button1.is_pressed:
+        print('button 1 pressed.')
+    if button2.is_pressed:
+        print('button 2 pressed.')
+    if button3.is_pressed:
+        print('button 3 pressed.')
