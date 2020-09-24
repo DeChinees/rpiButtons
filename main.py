05af67856a7b4d89d8937397b13d7ddd519fe362
DeChinees <dchinon@yahoo.com>
2020-09-24 21:42:28 +0200

    add timer

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ button1 = Button(18)
 button2 = Button(23)
 button3 = Button(24)
 
+reboot_statement = "sudo shutdown -r -f now"
+shutdown_statement = "sudo shutdown -h"
+
+held_for = 0.0
+
 while True:
     if button1.is_pressed:
         print('button 1 pressed.')
     if button2.is_pressed:
-        print('button 2 pressed.')
+        held_for = max(held_for, button2.held_time + button2.hold_time)
+        print(f'button 2 pressed for ', held_for, ' seconds.')
     if button3.is_pressed:
         print('button 3 pressed.')
